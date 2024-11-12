602e68231747528005bb5f4f643daadd4a1caffa
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2024-11-12 12:19:18 +0100

    Initial Commit

---
+++ Evently/EventlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventlyApp.swift
+//  Evently
+//
+//  Created by Łukasz Janiszewski on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct EventlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
